634b502cfe5484d57d20bcbd0acddaba9d7097f3
nanda widiarta <nandawidiarta@gmail.com>
2025-03-21 16:34:10 +0700

    add allowed headers in cors

--- src/application/web.ts
+++ src/application/web.ts
@@ -13,7 +13,7 @@ web.use(
   cors({
     origin: "*", 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-api-token", "X-API-TOKEN"],
     credentials: true,
   })
 );
