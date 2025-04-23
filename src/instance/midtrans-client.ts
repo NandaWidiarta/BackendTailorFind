d97100e5e0675ddcf7c7706eb334134037b776a3
nanda widiarta <nandawidiarta@gmail.com>
2025-04-23 22:53:12 +0700

    move some variabel

--- src/instance/midtrans-client.ts
+++ src/instance/midtrans-client.ts
@@ -1,7 +1,10 @@
 const midtransClient = require('midtrans-client')
 
+const MIDTRANS_SERVER_KEY = process.env.MIDTRANS_SERVER_KEY
+const MIDTRANS_CLIENT_KEY = process.env.MIDTRANS_CLIENT_KEY
+
 export const snap = new midtransClient.Snap({
   isProduction: false,
-  serverKey: "SB-Mid-server-O05GMugLOjzLZ9hGbaDI-7xO",
-  clientKey: "SB-Mid-client-SVVwIHhEHRH0CS46"
+  serverKey: MIDTRANS_SERVER_KEY ?? "",
+  clientKey: MIDTRANS_CLIENT_KEY ?? ""
 })
--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -4,13 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const SUPABASE_ADMIN_KEY = process.env.SUPABASE_ADMIN_KEY
 
 export const supabase = createClient(
-  'https://xtyrxekcsaesyyopouhh.supabase.co', // Ganti dengan URL proyek Anda
-  SUPABASE_KEY ?? ""// Ganti dengan kunci publik Supabase
+  'https://xtyrxekcsaesyyopouhh.supabase.co', 
+  SUPABASE_KEY ?? ""
 );
 
 export const supabaseAdmin = createClient(
   'https://xtyrxekcsaesyyopouhh.supabase.co', 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0eXJ4ZWtjc2Flc3l5b3BvdWhoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNTU0Nzc3NCwiZXhwIjoyMDUxMTIzNzc0fQ.47K32v-2ccduFSzCM__qYxaUSUBBvVB8hLj386iW00o'
+  SUPABASE_ADMIN_KEY ?? ""
 )
